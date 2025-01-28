a3afcb000af0b92c978829e28fd966283bdb4c76
Sofi Dobriak <sofi.dobriak@gmail.com>
2025-01-28 15:47:35 +0200

    Task #2 done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -8,12 +8,55 @@ const refs = {
   button: document.querySelector('button'),
 };
 
+// Styles
+//-----------------------------------------------------------------
 refs.labels.forEach(label => label.classList.add('form-label'));
 refs.input.classList.add('form-input');
 refs.textArea.classList.add('form-textArea');
 refs.button.classList.add('button');
+//-----------------------------------------------------------------
 
 const formData = {
   email: '',
   message: '',
 };
+
+const localeStorageKey = 'feedback-form-state';
+const savedData = localStorage.getItem(localeStorageKey);
+
+if (savedData) {
+  try {
+    const parseSavedData = JSON.parse(savedData);
+
+    refs.input.value = parseSavedData.email ?? '';
+    refs.textArea.value = parseSavedData.message ?? '';
+  } catch (error) {
+    console.error('Error parsing saved data:', error.message);
+  }
+}
+
+refs.form.addEventListener('input', () => {
+  formData.email = refs.input.value.trim();
+  formData.message = refs.textArea.value.trim();
+
+  localStorage.setItem(localeStorageKey, JSON.stringify(formData));
+});
+
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const email = refs.input.value.trim();
+  const message = refs.textArea.value.trim();
+
+  if (!email || !message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  const submittedData = { email, message };
+
+  console.log(submittedData);
+  localStorage.removeItem(localeStorageKey);
+
+  refs.form.reset();
+});
